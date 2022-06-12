c7c0c2db5deb50a69c58c47b02957b590e76dd12
Vishal Choudhary <contactvishaltech@gmail.com>
2022-06-12 18:33:07 +0530

    variables

---
+++ variables.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "initial"
+	fmt.Println(a)
+
+	var b, c int = 21, 12
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "walrus"
+	fmt.Println(f)
+}
